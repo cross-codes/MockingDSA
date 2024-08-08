46a6155b69e8f6126698a583120f15873a68e286
Akshaj Rao <akshajrao2004@gmail.com>
2024-08-08 15:36:23 +0530

    feat(.): add new solutions in Introductory problems

---
+++ Introductory Problems/IncreasingArray.java
@@ -0,0 +1,167 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class IncreasingArray implements Runnable {
+
+  BufferedReader in;
+  PrintWriter out;
+  StringTokenizer tok = new StringTokenizer("");
+
+  public static void main(String[] args) {
+    new Thread(null, new IncreasingArray(), "", 256 * (1L << 20)).start();
+  }
+
+  public void run() {
+    try {
+      in = new BufferedReader(new InputStreamReader(System.in));
+      out = new PrintWriter(System.out);
+      solve();
+      in.close();
+      out.close();
+    } catch (Throwable t) {
+      t.printStackTrace(System.err);
+      System.exit(-1);
+    }
+  }
+
+  String readString() throws IOException {
+    while (!tok.hasMoreTokens()) {
+      tok = new StringTokenizer(in.readLine());
+    }
+    return tok.nextToken();
+  }
+
+  int readInt() throws IOException {
+    return Integer.parseInt(readString());
+  }
+
+  long readLong() throws IOException {
+    return Long.parseLong(readString());
+  }
+
+  double readDouble() throws IOException {
+    return Double.parseDouble(readString());
+  }
+
+  void solve() throws IOException {
+    int n = readInt();
+    long ans = 0, prev = readLong();
+
+    for (int i = 1; i < n; i++) {
+      long currNum = readLong(), diff = currNum - prev;
+
+      if (diff >= 0) prev = currNum;
+      else ans += -diff;
+    }
+
+    out.println(ans);
+  }
+
+  // Custom STDIN reader. Relatively faster than BufferedReader
+  // NOTE: Has a 64 byte limitation per line of input
+
+  static class StandardInputReader {
+    private final int BUFFER_SIZE = 1 << 16;
+    private DataInputStream din;
+    private byte[] buffer;
+    private int bufferPointer, bytesRead;
+
+    public StandardInputReader() {
+      din = new DataInputStream(System.in);
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+    }
+
+    public StandardInputReader(String file_name) throws IOException {
+      din = new DataInputStream(new FileInputStream(file_name));
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+    }
+
+    public String readLine() throws IOException {
+      byte[] buf = new byte[64];
+      int cnt = 0, c;
+      while ((c = read()) != -1) {
+        if (c == '\n') {
+          if (cnt != 0) {
+            break;
+          } else {
+            continue;
+          }
+        }
+        buf[cnt++] = (byte) c;
+      }
+      return new String(buf, 0, cnt);
+    }
+
+    public int nextInt() throws IOException {
+      int ret = 0;
+      byte c = read();
+      while (c <= ' ') {
+        c = read();
+      }
+      boolean neg = (c == '-');
+      if (neg) c = read();
+      do {
+        ret = ret * 10 + c - '0';
+      } while ((c = read()) >= '0' && c <= '9');
+
+      if (neg) return -ret;
+      return ret;
+    }
+
+    public long nextLong() throws IOException {
+      long ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = (c == '-');
+      if (neg) c = read();
+      do {
+        ret = ret * 10 + c - '0';
+      } while ((c = read()) >= '0' && c <= '9');
+      if (neg) return -ret;
+      return ret;
+    }
+
+    public double nextDouble() throws IOException {
+      double ret = 0, div = 1;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = (c == '-');
+      if (neg) c = read();
+
+      do {
+        ret = ret * 10 + c - '0';
+      } while ((c = read()) >= '0' && c <= '9');
+
+      if (c == '.') {
+        while ((c = read()) >= '0' && c <= '9') {
+          ret += (c - '0') / (div *= 10);
+        }
+      }
+
+      if (neg) return -ret;
+      return ret;
+    }
+
+    private void fillBuffer() throws IOException {
+      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+      if (bytesRead == -1) buffer[0] = -1;
+    }
+
+    private byte read() throws IOException {
+      if (bufferPointer == bytesRead) fillBuffer();
+      return buffer[bufferPointer++];
+    }
+
+    public void close() throws IOException {
+      if (din == null) return;
+      din.close();
+    }
+  }
+}
---
+++ Introductory Problems/Permutations.java
@@ -0,0 +1,179 @@
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Permutations implements Runnable {
+
+  StandardInputReader in;
+  PrintWriter out;
+  StringTokenizer tok = new StringTokenizer("");
+
+  public static void main(String[] args) {
+    new Thread(null, new Permutations(), "", 256 * (1L << 20)).start();
+  }
+
+  public void run() {
+    try {
+      in = new StandardInputReader();
+      out = new PrintWriter(System.out);
+      solve();
+      in.close();
+      out.close();
+    } catch (Throwable t) {
+      t.printStackTrace(System.err);
+      System.exit(-1);
+    }
+  }
+
+  String readString() throws IOException {
+    while (!tok.hasMoreTokens()) {
+      tok = new StringTokenizer(in.readLine());
+    }
+    return tok.nextToken();
+  }
+
+  int readInt() throws IOException {
+    return Integer.parseInt(readString());
+  }
+
+  long readLong() throws IOException {
+    return Long.parseLong(readString());
+  }
+
+  double readDouble() throws IOException {
+    return Double.parseDouble(readString());
+  }
+
+  void solve() throws IOException {
+    int n = readInt();
+
+    switch (n) {
+      case 1:
+        out.print(1);
+        break;
+
+      case 2:
+      case 3:
+        out.println("NO SOLUTION");
+        break;
+
+      case 4:
+        out.print("2 4 1 3");
+        break;
+
+      default:
+        if ((n & 1) == 0) {
+          for (int i = 1; i < n; i += 2) out.print(i + " ");
+          for (int j = 2; j <= n; j += 2) out.print(j + " ");
+        } else {
+          for (int i = 1; i <= n; i += 2) out.print(i + " ");
+          for (int j = 2; j < n; j += 2) out.print(j + " ");
+        }
+    }
+  }
+
+  // Custom STDIN reader. Relatively faster than BufferedReader
+  // NOTE: Has a 64 byte limitation per line of input
+
+  static class StandardInputReader {
+    private final int BUFFER_SIZE = 1 << 16;
+    private DataInputStream din;
+    private byte[] buffer;
+    private int bufferPointer, bytesRead;
+
+    public StandardInputReader() {
+      din = new DataInputStream(System.in);
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+    }
+
+    public StandardInputReader(String file_name) throws IOException {
+      din = new DataInputStream(new FileInputStream(file_name));
+      buffer = new byte[BUFFER_SIZE];
+      bufferPointer = bytesRead = 0;
+    }
+
+    public String readLine() throws IOException {
+      byte[] buf = new byte[64];
+      int cnt = 0, c;
+      while ((c = read()) != -1) {
+        if (c == '\n') {
+          if (cnt != 0) {
+            break;
+          } else {
+            continue;
+          }
+        }
+        buf[cnt++] = (byte) c;
+      }
+      return new String(buf, 0, cnt);
+    }
+
+    public int nextInt() throws IOException {
+      int ret = 0;
+      byte c = read();
+      while (c <= ' ') {
+        c = read();
+      }
+      boolean neg = (c == '-');
+      if (neg) c = read();
+      do {
+        ret = ret * 10 + c - '0';
+      } while ((c = read()) >= '0' && c <= '9');
+
+      if (neg) return -ret;
+      return ret;
+    }
+
+    public long nextLong() throws IOException {
+      long ret = 0;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = (c == '-');
+      if (neg) c = read();
+      do {
+        ret = ret * 10 + c - '0';
+      } while ((c = read()) >= '0' && c <= '9');
+      if (neg) return -ret;
+      return ret;
+    }
+
+    public double nextDouble() throws IOException {
+      double ret = 0, div = 1;
+      byte c = read();
+      while (c <= ' ') c = read();
+      boolean neg = (c == '-');
+      if (neg) c = read();
+
+      do {
+        ret = ret * 10 + c - '0';
+      } while ((c = read()) >= '0' && c <= '9');
+
+      if (c == '.') {
+        while ((c = read()) >= '0' && c <= '9') {
+          ret += (c - '0') / (div *= 10);
+        }
+      }
+
+      if (neg) return -ret;
+      return ret;
+    }
+
+    private void fillBuffer() throws IOException {
+      bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+      if (bytesRead == -1) buffer[0] = -1;
+    }
+
+    private byte read() throws IOException {
+      if (bufferPointer == bytesRead) fillBuffer();
+      return buffer[bufferPointer++];
+    }
+
+    public void close() throws IOException {
+      if (din == null) return;
+      din.close();
+    }
+  }
+}
