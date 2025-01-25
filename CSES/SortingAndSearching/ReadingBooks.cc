3803170056ca2439b59d417751184fdf9185bcce
Akshaj Rao <akshajrao2004@gmail.com>
2025-01-26 01:48:16 +0530

    New shit

---
+++ CSES/SortingAndSearching/ReadingBooks.cc
@@ -0,0 +1,19 @@
+#ifdef CROSS
+#define _GLIBCXX_DEBUG 1
+#define _GLIBCXX_DEBUG_PEDANTIC 1
+#define _GLIBCXX_SANITIZE_VECTOR 1
+#endif
+
+#include <cstdint>
+#include <iostream>
+
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using u128 = unsigned __int128;
+
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+
+  return 0;
+}
