be5cfaa6700b2ed6d3a4f2e141ac4f635aede118
Akshaj Rao <akshajrao2004@gmail.com>
2025-01-27 21:36:40 +0530

    Add BoundedStack implementation

---
+++ lib/implementations/BoundedStack.hpp
@@ -0,0 +1,51 @@
+#include <cstddef>
+#include <cstring>
+#include <string>
+
+template <typename E> class BoundedStack {
+private:
+  int64_t STACK_SIZE_;
+  int64_t pos_ = -1;
+  E *stack_;
+
+public:
+  BoundedStack(size_t initialSize)
+      : STACK_SIZE_(initialSize) {
+    stack_ = new E[STACK_SIZE_];
+  };
+
+  ~BoundedStack() { delete[] stack_; }
+
+  void pop() {
+    if (pos_ == -1)
+      throw pos_;
+    pos_--;
+  }
+
+  void push(E element) {
+    if (pos_ >= STACK_SIZE_ - 1)
+      throw STACK_SIZE_;
+    stack_[++pos_] = element;
+  }
+
+  bool empty() { return pos_ == -1; }
+
+  size_t size() { return static_cast<size_t>(pos_ + 1); }
+
+  void clear() {
+    pos_ = -1;
+  }
+
+  E top() {
+    if (pos_ == -1)
+      throw pos_;
+    return stack_[pos_];
+  }
+
+  std::string to_string() {
+    std::string res = "[";
+    for (int i = 0; i <= pos_; i++)
+      res += std::to_string(stack_[i]) + (i == pos_ ? "" : ", ");
+    return res + "]";
+  }
+};
