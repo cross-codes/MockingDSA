2d667fe7caf9af9d7c067eb40f32ab81412dd068
Akshaj Rao <akshajrao2004@gmail.com>
2025-01-27 19:10:28 +0530

    New solutions

---
+++ CSES/SortingAndSearching/SlidingWindowCost.cc
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using u128 = unsigned __int128;
+
+int main() {
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+
+  size_t n, k;
+  std::cin >> n >> k;
+
+  std::vector<int> input(n);
+  for (size_t i = 0; i < n; i++)
+    std::cin >> input[i];
+
+  u64 cost = 0U;
+  for (size_t start = 0; start + k <= n; start++) {
+    std::vector<int> subVector(input.begin() + start,
+                               input.begin() + start + k);
+    std::sort(subVector.begin(), subVector.end());
+    int median = subVector[subVector.size() >> 1];
+
+    for (const auto &num : subVector)
+      cost += std::abs(num - median);
+
+    std::cout << cost << " ";
+    cost = 0U;
+  }
+
+  std::cout << "\n";
+
+  return 0;
+}
