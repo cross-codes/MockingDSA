85951375f1512cfe50d3ac23cc720a3561bbac3d
Akshaj Rao <akshajrao2004@gmail.com>
2025-02-02 11:52:09 +0530

    Finish Introductory Problems!

--- CSES/IntroductoryProblems/GridPaths.cc
+++ CSES/IntroductoryProblems/GridPaths.cc
@@ -1,7 +1,6 @@
 #include <cstdint>
+#include <cstdio>
 #include <cstring>
-#include <iostream>
-#include <string>
 
 using i64 = int64_t;
 using u64 = uint64_t;
@@ -15,86 +14,82 @@ bool visited[N + 1][N + 1];
 
 enum class Direction { UP, DOWN, LEFT, RIGHT, NONE };
 
-void search(size_t, size_t, size_t, std::string, Direction);
-auto inBounds(size_t x, size_t y) -> bool;
+inline void search(size_t, size_t, size_t, char pattern[], Direction);
+inline auto inBounds(size_t x, size_t y) -> bool;
+inline auto canGo(Direction dir, size_t x, size_t y) -> bool;
 
 int main() {
-  std::cin.tie(nullptr)->sync_with_stdio(false);
 
-  std::string pattern;
-  std::cin >> pattern;
+  char pattern[N * N];
+  std::scanf("%s", pattern);
 
-  search(1, 1, 0, pattern, Direction::NONE);
+  search(1, 1, 0, pattern, ::Direction::NONE);
 
-  std::cout << cnt << "\n";
+  std::printf("%ld\n", ::cnt);
 
   return 0;
 }
 
-auto inBounds(size_t x, size_t y) -> bool {
+inline auto inBounds(size_t x, size_t y) -> bool {
   return (x >= 1) && (y >= 1) && (x <= N) && (y <= N);
 }
 
-void search(size_t x, size_t y, size_t idx, std::string pattern,
-            Direction dir) {
+inline auto canGo(::Direction dir, size_t x, size_t y) -> bool {
+  switch (dir) {
+  case ::Direction::UP:
+    return ::inBounds(x, y - 1) && !::visited[y - 1][x];
+
+  case ::Direction::RIGHT:
+    return ::inBounds(x + 1, y) && !::visited[y][x + 1];
+
+  case ::Direction::DOWN:
+    return ::inBounds(x, y + 1) && !::visited[y + 1][x];
+
+  case ::Direction::LEFT:
+    return ::inBounds(x - 1, y) && !::visited[y][x - 1];
+
+  default:
+    return false;
+  }
+}
+
+inline void search(size_t x, size_t y, size_t idx, char pattern[],
+                   ::Direction dir) {
   if (idx == N * N - 1 || (x == 1 && y == N)) {
     if (idx == N * N - 1 && x == 1 && y == N)
-      cnt++;
+      ::cnt++;
 
     return;
   }
 
-  if (dir == Direction::UP) {
-    if (!inBounds(x, y - 1) || visited[y - 1][x])
-      if (inBounds(x - 1, y) && !visited[y][x - 1] && inBounds(x + 1, y) &&
-          !visited[y][x + 1])
+  if (!::canGo(dir, x, y)) {
+    if (dir == ::Direction::UP || dir == ::Direction::DOWN) {
+      if (::canGo(::Direction::RIGHT, x, y) && ::canGo(::Direction::LEFT, x, y))
         return;
-  } else if (dir == Direction::DOWN) {
-    if (!inBounds(x, y + 1) || visited[y + 1][x])
-      if (inBounds(x - 1, y) && !visited[y][x - 1] && inBounds(x + 1, y) &&
-          !visited[y][x + 1])
+    } else if (dir == ::Direction::RIGHT || dir == ::Direction::LEFT) {
+      if (::canGo(::Direction::UP, x, y) && ::canGo(::Direction::DOWN, x, y))
         return;
-  } else if (dir == Direction::RIGHT) {
-    if (!inBounds(x + 1, y) || visited[y][x + 1])
-      if (inBounds(x, y - 1) && !visited[y - 1][x] && inBounds(x, y + 1) &&
-          !visited[y + 1][x])
-        return;
-  } else if (dir == Direction::LEFT) {
-    if (!inBounds(x - 1, y) || visited[y][x - 1])
-      if (inBounds(x, y + 1) && !visited[y + 1][x] && inBounds(x, y - 1) &&
-          !visited[y - 1][x])
-        return;
+    }
   }
 
-  visited[y][x] = true;
+  ::visited[y][x] = true;
   char c = pattern[idx];
 
-  if (c == '?') {
-    if (inBounds(x, y + 1) && !visited[y + 1][x])
-      search(x, y + 1, idx + 1, pattern, Direction::DOWN);
+  if (c == 'D' || c == '?')
+    if (::inBounds(x, y + 1) && !::visited[y + 1][x])
+      search(x, y + 1, idx + 1, pattern, ::Direction::DOWN);
 
-    if (inBounds(x, y - 1) && !visited[y - 1][x])
+  if (c == 'U' || c == '?')
+    if (::inBounds(x, y - 1) && !::visited[y - 1][x])
       search(x, y - 1, idx + 1, pattern, Direction::UP);
 
-    if (inBounds(x + 1, y) && !visited[y][x + 1])
-      search(x + 1, y, idx + 1, pattern, Direction::RIGHT);
+  if (c == 'R' || c == '?')
+    if (::inBounds(x + 1, y) && !::visited[y][x + 1])
+      search(x + 1, y, idx + 1, pattern, ::Direction::RIGHT);
 
-    if (inBounds(x - 1, y) && !visited[y][x - 1])
-      search(x - 1, y, idx + 1, pattern, Direction::LEFT);
+  if (c == 'L' || c == '?')
+    if (::inBounds(x - 1, y) && !::visited[y][x - 1])
+      search(x - 1, y, idx + 1, pattern, ::Direction::LEFT);
 
-  } else if (c == 'D') {
-    if (inBounds(x, y + 1) && !visited[y + 1][x])
-      search(x, y + 1, idx + 1, pattern, Direction::DOWN);
-  } else if (c == 'U') {
-    if (inBounds(x, y - 1) && !visited[y - 1][x])
-      search(x, y - 1, idx + 1, pattern, Direction::UP);
-  } else if (c == 'R') {
-    if (inBounds(x + 1, y) && !visited[y][x + 1])
-      search(x + 1, y, idx + 1, pattern, Direction::RIGHT);
-  } else {
-    if (inBounds(x - 1, y) && !visited[y][x - 1])
-      search(x - 1, y, idx + 1, pattern, Direction::LEFT);
-  }
-
-  visited[y][x] = false;
+  ::visited[y][x] = false;
 }
