d3daeb9646f9f3f71e6c0811f61549770cf04ee6
Akshaj Rao <akshajrao2004@gmail.com>
2025-02-09 23:06:53 +0530

    New solutions

---
+++ Codeforces/practice/2000-2099/2065A.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+#ifdef CROSS
+#include "util/Debug.hh"
+#else
+#define dbg(...) 249
+#endif
+
+using usize = size_t;
+using ssize = ptrdiff_t;
+
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using u128 = unsigned __int128;
+
+auto run() -> void {
+  std::string input;
+  std::cin >> input;
+
+  if (input.size() <= 2) {
+    std::println("i");
+  } else {
+    std::println("{}", input.substr(0, input.size() - 2) + "i");
+  }
+}
+
+int main() {
+#ifdef CROSS
+  FILE *stream = std::freopen("input.txt", "r", stdin);
+  if (stream == nullptr) {
+    std::println(stderr, "Input file not found");
+    __builtin_trap();
+  }
+#else
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+#endif
+
+  int t;
+  std::cin >> t;
+
+  while (t-- > 0)
+    run();
+
+#ifdef CROSS
+  std::fclose(stdin);
+#endif
+
+  return 0;
+}
---
+++ Codeforces/practice/2000-2099/2065C2.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+#ifdef CROSS
+#include "util/Debug.hh"
+#else
+#define dbg(...) 249
+#endif
+
+using usize = size_t;
+using ssize = ptrdiff_t;
+
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using u128 = unsigned __int128;
+
+auto run() -> void {
+  ssize n, m;
+  std::cin >> n >> m;
+
+  std::unique_ptr<int[]> a(new int[n]);
+  std::vector<int> b(m);
+
+  for (ssize i = 0; i < n; i++)
+    std::cin >> a[i];
+
+  for (ssize i = 0; i < m; i++)
+    std::cin >> b[i];
+
+  std::sort(b.begin(), b.end());
+
+  if (b[0] - a[0] < a[0])
+    a[0] = b[0] - a[0];
+
+  for (ssize i = 1; i < n - 1; i++) {
+    auto it = std::lower_bound(b.begin(), b.end(), a[i] + a[i - 1]);
+    if (it != b.end()) {
+      if (a[i] >= a[i - 1]) {
+        if (*it - a[i] < a[i])
+          a[i] = *it - a[i];
+      } else {
+        a[i] = *it - a[i];
+      }
+    }
+  }
+
+  if (b[m - 1] - a[n - 1] > a[n - 1])
+    a[n - 1] = b[m - 1] - a[n - 1];
+
+  for (ssize i = 0; i < n - 1; i++) {
+    if (a[i] > a[i + 1]) {
+      std::println("NO");
+      return;
+    }
+  }
+
+  std::println("YES");
+}
+
+int main() {
+#ifdef CROSS
+  FILE *stream = std::freopen("input.txt", "r", stdin);
+  if (stream == nullptr) {
+    std::println(stderr, "Input file not found");
+    __builtin_trap();
+  }
+#else
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+#endif
+
+  int t;
+  std::cin >> t;
+
+  while (t-- > 0)
+    run();
+
+#ifdef CROSS
+  std::fclose(stdin);
+#endif
+
+  return 0;
+}
