0d7ed7f5deb1306cba4616de1612d4643a1f168f
Akshaj Rao <akshajrao2004@gmail.com>
2025-02-11 18:56:07 +0530

    Fix wrong solution

--- Codeforces/practice/2000-2099/2065D.cc
+++ Codeforces/practice/2000-2099/2065D.cc
@@ -14,58 +14,36 @@ using u64 = uint64_t;
 using u32 = uint32_t;
 using u128 = unsigned __int128;
 
-auto constexpr VECTOR_ORDER =
-    [](const std::pair<std::vector<i64>, std::vector<__int128>> &a,
-       const std::pair<std::vector<i64>, std::vector<__int128>> &b) -> bool {
-  const auto &sumsA = a.second;
-  const auto &sumsB = b.second;
-
-  for (usize i = 0; i < sumsA.size(); i++) {
-    __int128 cmp = sumsA[i] - sumsB[i];
-    if (cmp != 0)
-      return cmp > 0;
-  }
-
-  return false;
+inline auto constexpr VECTOR_ORDER =
+    [](const std::pair<std::vector<i64>, i64> &a,
+       const std::pair<std::vector<i64>, i64> &b) -> bool {
+  return a.second > b.second;
 };
 
 auto run() -> void {
   ssize n, m;
   std::cin >> n >> m;
 
-  std::vector<std::pair<std::vector<i64>, std::vector<__int128>>> arrays(
-      n, std::make_pair(std::vector<i64>(m), std::vector<__int128>(m)));
+  std::vector<std::pair<std::vector<i64>, i64>> arrays(
+      n, std::make_pair(std::vector<i64>(m), 0LL));
 
   for (ssize i = 0; i < n; i++) {
-    __int128 specSum = 0, cnt = m + 1;
-    for (ssize j = 0; j < m; j++) {
-      int num;
-      std::cin >> num;
-      specSum += cnt * num;
-      (arrays[i].first)[j] = num;
-      cnt--;
-    }
+    i64 &totSum = arrays[i].second;
+    auto &vec = arrays[i].first;
 
-    auto &ref = arrays[i].first;
-    auto &array = arrays[i].second;
-
-    __int128 curSum = specSum;
-    cnt = m + 1;
     for (ssize j = 0; j < m; j++) {
-      array[j] = curSum;
-      curSum -= (cnt)*ref[j];
-      cnt--;
+      std::cin >> vec[j];
+      totSum += vec[j];
     }
   }
 
   std::sort(arrays.begin(), arrays.end(), ::VECTOR_ORDER);
 
-  u128 totSum = 0, cnt = n * m;
+  u64 totSum = 0ULL, cnt = n * m;
   for (ssize i = 0; i < n; i++) {
-    for (ssize j = 0; j < m; j++) {
-      totSum += (arrays[i].first)[j] * cnt;
-      cnt--;
-    }
+    auto &vec = arrays[i].first;
+    for (ssize j = 0; j < m; j++)
+      totSum += vec[j] * cnt--;
   }
 
   std::println("{}", totSum);
