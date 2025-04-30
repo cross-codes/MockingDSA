b1e7a131814b176d210451bbe53986336406614d
Akshaj Rao <akshajrao2004@gmail.com>
2025-04-30 13:02:37 +0530

    New solutions

---
+++ CSES_Final/rust/src/bin/MovieFestival.rs
@@ -0,0 +1,100 @@
+use std::error::Error;
+use std::io::{self, BufRead, BufWriter, Write};
+
+fn run(
+  scanner: &mut Scanner<io::StdinLock>,
+  writer: &mut BufWriter<io::StdoutLock>,
+)
+{
+  macro_rules! print {
+        () => {
+            writeln!(writer).unwrap();
+        };
+        ($arg:expr) => {
+            write!(writer, "{}", $arg).unwrap();
+        };
+        ($arg:expr, $($rest:expr),*) => {
+            write!(writer, "{}", $arg).unwrap();
+            print!($($rest),*);
+        };
+    }
+
+  let n: usize = scanner.next();
+  let mut times: Vec<(i32, i32)> =
+    (0..n).map(|_| (scanner.next(), scanner.next())).collect();
+
+  times.sort_unstable_by(|a, b| a.1.cmp(&b.1).then_with(|| a.0.cmp(&b.0)));
+
+  let mut max_movies: i32 = 0;
+  let mut current_end: i32 = 0;
+  for i in 0..n
+  {
+    if times[i].0 >= current_end
+    {
+      current_end = times[i].1;
+      max_movies += 1;
+    }
+  }
+
+  print!(max_movies, "\n");
+}
+
+struct Scanner<B>
+{
+  reader: B,
+  buffer: Vec<String>,
+  pos: usize,
+}
+
+impl<B: BufRead> Scanner<B>
+{
+  fn new(reader: B) -> Self
+  {
+    Self {
+      reader,
+      buffer: Vec::new(),
+      pos: 0,
+    }
+  }
+
+  fn next<T: std::str::FromStr>(&mut self) -> T
+  where
+    T::Err: std::fmt::Debug,
+  {
+    self.ensure_buffered();
+    let val = self.buffer[self.pos].parse().unwrap();
+    self.pos += 1;
+    val
+  }
+
+  fn ensure_buffered(&mut self)
+  {
+    if self.pos >= self.buffer.len()
+    {
+      let mut line = String::new();
+      self.reader.read_line(&mut line).unwrap();
+      self.buffer = line.split_whitespace().map(|s| s.to_owned()).collect();
+      self.pos = 0;
+    }
+  }
+}
+
+fn main() -> Result<(), Box<dyn Error>>
+{
+  let stdin = io::stdin();
+  let stdout = io::stdout();
+  let mut scanner = Scanner::new(stdin.lock());
+  let mut writer = BufWriter::new(stdout.lock());
+
+  #[allow(unused_assignments)]
+  let t: i32 = 1;
+
+  for _ in 0..t
+  {
+    run(&mut scanner, &mut writer);
+  }
+
+  writer.flush().unwrap();
+
+  Ok(())
+}
