d264b9af5ab346e1d5704a9a34ecce038d981f12
Akshaj Rao <akshajrao2004@gmail.com>
2025-06-16 20:43:38 +0530

    New solutions

---
+++ Sheets/src/striver_sde/recursion-and-backtracking/_1_PrintAllPermutationsOfAnArray.cc
@@ -0,0 +1,15 @@
+#include <algorithm>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> permute(std::vector<int> &nums) {
+    std::vector<std::vector<int>> res;
+    std::sort(nums.begin(), nums.end());
+    do {
+      res.push_back(nums);
+    } while (std::next_permutation(nums.begin(), nums.end()));
+
+    return res;
+  }
+};
